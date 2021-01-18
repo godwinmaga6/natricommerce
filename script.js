3c611c1cb3373b33dbcb1567693b5a0748ced067
Godwin Maga <godwinmaga6@gmail.com>
2021-01-19 00:47:21 +0100

    Added sign up modal

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 function togglePopup(){
     document.getElementById('popup-1').classList.toggle('active');
 }
+
+function toggleSignup(){
+    document.getElementById('popup-2').classList.toggle('active');
+}
