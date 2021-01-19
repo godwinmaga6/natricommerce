96b031f0bd9d3b76eac46fa2fb07e792056d4657
Godwin Maga <godwinmaga6@gmail.com>
2021-01-19 01:41:12 +0100

    Genaral upates

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
+const LOGINMODAL = document.getElementById('popup-1');
+const SIGNUPMOAL = document.getElementById('popup-2');
+const OVERLAY = document.querySelectorAll('.overlay');
+
+OVERLAY.forEach((layer)=> {
+    layer.addEventListener('click', () => {
+        if(LOGINMODAL.classList.contains('active')){
+            LOGINMODAL.classList.remove('active');
+        }
+    
+       if(SIGNUPMOAL.classList.contains('active')){
+            SIGNUPMOAL.classList.remove('active');
+       }
+    })
+})
+
+let meanMenu = document.querySelector('.meanmenu-reveal');
 function togglePopup(){
-    document.getElementById('popup-1').classList.toggle('active');
+   LOGINMODAL.classList.toggle('active');
+   meanMenu.classList.remove('meanClose');
 }
 
 function toggleSignup(){
-    document.getElementById('popup-2').classList.toggle('active');
+    SIGNUPMOAL.classList.toggle('active');
+    meanMenu.classList.remove('meanClose');
 }
